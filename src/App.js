5fbc8d8626172d619b494b67ec5468c094375f6f
Vitaliy <itvkip@yandex.ru>
2020-12-02 16:34:53 +0300

    Added hooks for inputs;

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const[name, setName] = useState('Hello');
+  const[age, setAge] = useState(0);
+  const[country, setCountry] = useState('');
+  const[position, setPosition] = useState('');
+  const[wage, setWage] = useState(0);
+
   return (
     <div className="App">
       <div>
       	<label>Name:</label>
-      	<input type='text'/>
+      	<input type='text' onChange={ (event) => { setName(event.target.value) } }/>
       	<label>Age:</label>
-      	<input type='number'/>
+      	<input type='number'  onChange={ (event) => { setAge(event.target.value) } }/>
       	<label>Country:</label>
-      	<input type='text'/>
+      	<input type='text'  onChange={ (event) => { setCountry(event.target.value) } }/>
       	<label>Position:</label>
-      	<input type='text'/>
+      	<input type='text'  onChange={ (event) => { setPosition(event.target.value) } }/>
       	<label>Wage (month):</label>
-      	<input type='number'/>
+      	<input type='number'  onChange={ (event) => { setWage(event.target.value) } }/>
+        <button>Add</button>
       </div>
     </div>
   );
